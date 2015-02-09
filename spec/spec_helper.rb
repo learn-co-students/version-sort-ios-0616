81e6aa332771f9d9bb1faaeb38e274ac6ce65b63
Katie Hoffman <ktahoffman@gmail.com>
2015-02-09 14:27:52 -0500

    fix tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require_relative '../version_sort.rb'
-
+require 'json'
 RSpec.configure do |config|
   # config here
 end
--- spec/version_sort_spec.rb
+++ spec/version_sort_spec.rb
@@ -1,70 +1,36 @@
 describe "#version sort" do
 
-  let(:unsorted_versions) { 
-    [
-      "foo-1.10.2.ext",
-      "foo-1.11.ext",
-      "foo-1.3.ext",
-      "foo-1.50.ext",
-      "foo-1.8.7.ext",
-      "foo-1.9.3.ext",
-      "foo-1.ext",
-      "foo-10.1.ext",
-      "foo-10.ext",
-      "foo-100.ext",
-      "foo-13.ext",
-      "foo-2.0.0.ext",
-      "foo-2.0.1.ext",
-      "foo-2.0.ext",
-      "foo-2.007.ext",
-      "foo-2.01.ext",
-      "foo-2.012b.ext",
-      "foo-2.01a.ext",
-      "foo-2.0a.ext",
-      "foo-2.0b.ext",
-      "foo-2.1.ext",
-      "foo-25.ext",
-      "foo-6.ext"
-    ] 
-  }
-  
-  let(:sorted_versions) { 
-    [
-      "foo-1.ext",
-      "foo-1.3.ext",
-      "foo-1.8.7.ext",
-      "foo-1.9.3.ext",
-      "foo-1.10.2.ext",
-      "foo-1.11.ext",
-      "foo-1.50.ext",
-      "foo-2.0.ext",
-      "foo-2.0a.ext",
-      "foo-2.0b.ext",
-      "foo-2.0.0.ext",
-      "foo-2.0.1.ext",
-      "foo-2.01.ext",
-      "foo-2.01a.ext",
-      "foo-2.1.ext",
-      "foo-2.007.ext",
-      "foo-2.012b.ext",
-      "foo-6.ext",
-      "foo-10.ext",
-      "foo-10.1.ext",
-      "foo-13.ext",
-      "foo-25.ext",
-      "foo-100.ext"
-    ]
-  }
+  context "simple" do
+    let(:unsorted_versions) { ["foo-25.ext", "foo-6.ext", "foo-100.ext", "foo-1.ext"]}
+    let(:sorted_versions) { ["foo-1.ext", "foo-6.ext", "foo-25.ext", "foo-100.ext"] }
 
-  it "turns an array of unsorted files into an array of sorted files" do
-    result = unsorted_versions.version_sort
-    puts "\nDesired Order:"
-    puts sorted_versions
-    puts "\nYour Order:"
-    puts result
+    it "turns an array of unsorted files into an array of sorted files" do
+      expect(unsorted_versions.version_sort).to eq(sorted_versions)
+    end
+  end
 
-    sorted_versions.each_with_index do |version, i|
-      expect(result[i]).to eq(version)
+  context "medium" do
+    let(:unsorted_versions) {  ["foo-7.0b.ext", "foo-7.0.ext", "foo-6.0.ext", "foo-7.0a.ext"] }
+    let(:sorted_versions) { [ "foo-6.0.ext", "foo-7.0.ext", "foo-7.0a.ext", "foo-7.0b.ext"] }
+
+    it "turns an array of unsorted files into an array of sorted files" do
+      expect(unsorted_versions.version_sort).to eq(sorted_versions)
+    end
+  end
+
+  context "complex" do
+    let(:unsorted_versions) {  JSON.parse(File.read("spec/fixtures/unsorted.json"))}
+    let(:sorted_versions) { JSON.parse(File.read("spec/fixtures/sorted.json")) }
+    let(:result) { unsorted_versions.version_sort }
+
+    it "detail: turns an array of unsorted files into an array of sorted files" do
+      sorted_versions.each_with_index do |version, i|
+        expect(result[i]).to eq(version)
+      end
+    end
+
+    it "overview: turns an array of unsorted files into an array of sorted files" do
+      expect(result).to eq(sorted_versions)
     end
   end
 
