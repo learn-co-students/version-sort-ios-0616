14d967cb1e15e0a6e09e98a49a3c38c9bf6e9da4
Katie Hoffman <ktahoffman@gmail.com>
2015-03-21 12:45:12 -0400

    rearrange files

--- lib/version_sort.rb
+++ lib/version_sort.rb
@@ -1,13 +1,1 @@
-class Array
-
-  REGEX = /^(.*?)([\d.]*[\d])(.*?)$/
-  
-  def version_sort
-    self.sort_by do |version|
-      _, prefix, numbers, suffix = REGEX.match(version).to_a
-      numbers = numbers.split(".").map(&:to_i)
-      period = suffix.start_with?(".") ? 0 : 1
-      [prefix, numbers, period, suffix]
-    end
-  end 
-end
+# your code here
